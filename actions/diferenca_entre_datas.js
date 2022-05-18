7ea1bfd95041d7522a854e2eef19decc69f567cc
TempestDBM <48209160+TempestDBM@users.noreply.github.com>
2022-05-18 18:39:03 -0300

    Add files via upload

--- actions/diferenca_entre_datas.js
+++ actions/diferenca_entre_datas.js
@@ -1,10 +1,18 @@
 module.exports = {
 
-    name: 'Diferença entre Datas',
+    name: 'Calcular Diferença entre Datas',
     section: 'Other Stuff',
 
+    meta: { 
+      version: "2.1.2", 
+      preciseCheck: true, 
+      author: "Tempest#8741", 
+      authorUrl: null, 
+      downloadUrl: "https://github.com/TempestDBM"
+    },
+
     subtitle(data) {
-      return `Calcular ${data.time}`;
+      return `Diferença entre ${data.date} e ${data.date2}`;
     },
   
     variableStorage(data, varType) {
@@ -12,19 +20,30 @@ module.exports = {
       return [data.varName, 'Date'];
     },
   
-    fields: ['time', 'storage', 'varName'],
+    fields: ['date', 'date2', 'saida', 'storage', 'varName'],
   
     html(_isEvent, data) {
       return `
   <span>Ação feita por: Tempest#8741</span>
   <div style="float: left; width: 70%; padding-top: 10px; padding-bottom: 15px;">
-    Data inicial:
-    <input id="time" class="round" type="text" placeholder="22/02/2022 22:22:22">
+    Data 1:
+    <input id="date" class="round" type="text" placeholder='22/02/2022 22:22:22 ou "new" para uma nova data.'>
+  </div>
+  <div style="float: left; width: 70%; padding-top: 10px; padding-bottom: 15px;">
+    Data 2:
+    <input id="date2" class="round" type="text" placeholder='22/02/2022 22:22:22 ou "new" para uma nova data.'>
+  </div>
+  <div style="float: left; width: 70%; padding-top: 10px; padding-bottom: 15px;">
+  Converter segundos:
+  <select id="saida" class="round">
+      <option value="0" selected>Sim</option>
+      <option value="1">Não</option>
+  </select>
   </div>
   <div style="float: left; width: 35%; padding-top: 8px;">
     Store Result In:<br>
     <select id="storage" class="round" onchange="glob.variableChange(this, 'varNameContainer')">
-    ${data.variables[0]}
+    ${data.variables[1]}
     </select>
   </div>
   <div id="varNameContainer" style="float: right; display: none; width: 60%; padding-top: 8px;">
@@ -39,14 +58,48 @@ module.exports = {
     },
   
     async action(cache) {
-      const data = cache.actions[cache.index];
-      const time = this.evalMessage(data.time, cache);
-      const moment = require('moment');
+        const data = cache.actions[cache.index];
+        const moment = require('moment');
 
-        const d1 = time;
-        const d2 = new Date();
+        const date = this.evalMessage(data.date, cache);
+        const date2 = this.evalMessage(data.date2, cache);
+        const saida = parseInt(data.saida, 10);
+
+        var d1 = date;
+        var d2 = date2;
+
+        if(d1 == "new") {
+          d1 = new Date();
+        }
+
+        if(d2 == "new") {
+          d2 = new Date();
+        }
 
         result = moment(d2, 'DD/MM/YYYY HH:mm:ss').diff(moment(d1, 'DD/MM/YYYY HH:mm:ss'), 'seconds');
+
+        switch (saida) {
+          case 0:
+            if(isNaN(result) || result == "Invalid date") {
+              result = "Data inválida!";
+            } else {
+              let s = result;  
+              let m = Math.floor(s / 60);
+              s %= 60;
+              let h = Math.floor(m / 60);
+              m %= 60;
+              const d = Math.floor(h / 24);
+              h %= 24;
+          
+              result = `${d}d ${h}h ${m}m ${s}s`;
+            }
+            break;
+          case 1:
+            if(isNaN(result) || result == "Invalid date") {
+              result = "Data inválida!";
+            }
+            break;
+        }
 
         const storage = parseInt(data.storage, 10);
         const varName = this.evalMessage(data.varName, cache);
