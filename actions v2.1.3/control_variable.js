216f39da462f9d23659639914bfa8aa3fdc3d195
TempestDBM <48209160+TempestDBM@users.noreply.github.com>
2022-05-27 19:03:13 -0300

    Add files via upload

--- actions v2.1.3/control_variable.js
+++ actions v2.1.3/control_variable.js
@@ -28,11 +28,6 @@ module.exports = {
     }`;
   },
 
-  //---------------------------------------------------------------------
-  // Action Storage Function
-  //
-  // Stores the relevant variable info for the editor.
-  //---------------------------------------------------------------------
 
   variableStorage(data, varType) {
     const type = parseInt(data.storage, 10);
@@ -40,77 +35,41 @@ module.exports = {
     return [data.varName, "Unknown Type"];
   },
 
-  //---------------------------------------------------------------------
-  // Action Meta Data
-  //
-  // Helps check for updates and provides info if a custom mod.
-  // If this is a third-party mod, please set "author" and "authorUrl".
-  //
-  // It's highly recommended "preciseCheck" is set to false for third-party mods.
-  // This will make it so the patch version (0.0.X) is not checked.
-  //---------------------------------------------------------------------
 
-  meta: { version: "2.1.3", preciseCheck: true, author: null, authorUrl: null, downloadUrl: null },
+  meta: { version: "2.1.2", preciseCheck: true, author: "XinXyla", authorUrl: null, downloadUrl: null },
 
-  //---------------------------------------------------------------------
-  // Action Fields
-  //
-  // These are the fields for the action. These fields are customized
-  // by creating elements with corresponding IDs in the HTML. These
-  // are also the names of the fields stored in the action's JSON data.
-  //---------------------------------------------------------------------
 
   fields: ["storage", "varName", "changeType", "value"],
 
-  //---------------------------------------------------------------------
-  // Command HTML
-  //
-  // This function returns a string containing the HTML used for
-  // editing actions.
-  //
-  // The "isEvent" parameter will be true if this action is being used
-  // for an event. Due to their nature, events lack certain information,
-  // so edit the HTML to reflect this.
-  //---------------------------------------------------------------------
 
   html(isEvent, data) {
     return `
-<store-in-variable dropdownLabel="Store In" selectId="storage" variableContainerId="varNameContainer" variableInputId="varName"></store-in-variable>
+<store-in-variable dropdownLabel="Armazenado em" selectId="storage" variableContainerId="varNameContainer" variableInputId="varName"></store-in-variable>
 
 <br><br><br>
 
 <div style="padding-top: 8px;">
 	<div style="float: left; width: 35%;">
-		<span class="dbminputlabel">Control Type</span><br>
+		<span class="dbminputlabel">Tipo de controle</span><br>
 		<select id="changeType" class="round">
-			<option value="0" selected>Set Value</option>
-			<option value="1">Add Value</option>
+			<option value="0" selected>Alterar valor</option>
+			<option value="1">Adicionar valor</option>
 		</select>
+	</div></div>
+
+  <br><br><br>
+
+	<div style="float: left; width: 100%;">
+		<span class="dbminputlabel">Valor</span><br>
+    <textarea id="value" rows="6" class="round" name="is-eval" style="width:100%"></textarea><br>
 	</div>
-	<div style="float: right; width: 60%;">
-		<span class="dbminputlabel">Value</span><br>
-		<input id="value" class="round" type="text" name="is-eval"><br>
-	</div>
-</div>`;
+`;
   },
 
-  //---------------------------------------------------------------------
-  // Action Editor Init Code
-  //
-  // When the HTML is first applied to the action editor, this code
-  // is also run. This helps add modifications or setup reactionary
-  // functions for the DOM elements.
-  //---------------------------------------------------------------------
+
 
   init() {},
 
-  //---------------------------------------------------------------------
-  // Action Bot Function
-  //
-  // This is the function for the action within the Bot's Action class.
-  // Keep in mind event calls won't have access to the "msg" parameter,
-  // so be sure to provide checks for variable existence.
-  //---------------------------------------------------------------------
 
   action(cache) {
     const data = cache.actions[cache.index];
@@ -140,14 +99,6 @@ module.exports = {
     this.callNextAction(cache);
   },
 
-  //---------------------------------------------------------------------
-  // Action Bot Mod
-  //
-  // Upon initialization of the bot, this code is run. Using the bot's
-  // DBM namespace, one can add/modify existing functions if necessary.
-  // In order to reduce conflicts between mods, be sure to alias
-  // functions you wish to overwrite.
-  //---------------------------------------------------------------------
 
   mod() {},
 };
