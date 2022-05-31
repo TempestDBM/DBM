8f57c4393e52a3161e64dd1309ea29bd6fed5c0c
TempestDBM <48209160+TempestDBM@users.noreply.github.com>
2022-05-31 19:52:45 -0300

    Add files via upload

--- actions v2.1.3/call_command_or_event.js
+++ actions v2.1.3/call_command_or_event.js
@@ -1,131 +1,153 @@
+/* eslint-disable no-unused-vars */
 module.exports = {
-  //---------------------------------------------------------------------
-  // Action Name
-  //
-  // This is the name of the action displayed in the editor.
-  //---------------------------------------------------------------------
+  name: 'Call Command/Event',
+  section: 'Other Stuff',
+  meta: { version: "2.1.3", preciseCheck: true, author: 'XinXyla', authorUrl: 'https://github.com/XinXyla/', downloadUrl: 'https://raw.githubusercontent.com/XinXyla/DBM-MODS-v2/main/actions/call_command_or_event.js' },
 
-  name: "Call Command/Event",
-
-  //---------------------------------------------------------------------
-  // Action Section
-  //
-  // This is the section the action will fall into.
-  //---------------------------------------------------------------------
-
-  section: "Other Stuff",
-
-  //---------------------------------------------------------------------
-  // Action Subtitle
-  //
-  // This function generates the subtitle displayed next to the name.
-  //---------------------------------------------------------------------
-
-  subtitle(data, presets) {
-    return `Call Command/Event ID "${data.source}"`;
+  subtitle(data) {
+    let source;
+    if (parseInt(data.sourcetype, 10) === 2) {
+      source = 'Nome do comando: ' + data.source3.toString();
+    }
+    if (parseInt(data.sourcetype, 10) === 1) {
+      source = 'ID do comando: ' +data.source2.toString();
+    } 
+    if (parseInt(data.sourcetype, 10) === 0) {
+      source = 'Lista > comando: ' +data.source.toString();
+    }
+    return `${source}`;
   },
 
-  //---------------------------------------------------------------------
-  // Action Meta Data
-  //
-  // Helps check for updates and provides info if a custom mod.
-  // If this is a third-party mod, please set "author" and "authorUrl".
-  //
-  // It's highly recommended "preciseCheck" is set to false for third-party mods.
-  // This will make it so the patch version (0.0.X) is not checked.
-  //---------------------------------------------------------------------
+  fields: ['sourcetype', 'source', 'source2', 'source3', 'type'],
 
-  meta: { version: "2.1.3", preciseCheck: true, author: null, authorUrl: null, downloadUrl: null },
-
-  //---------------------------------------------------------------------
-  // Action Fields
-  //
-  // These are the fields for the action. These fields are customized
-  // by creating elements with corresponding IDs in the HTML. These
-  // are also the names of the fields stored in the action's JSON data.
-  //---------------------------------------------------------------------
-
-  fields: ["source", "type"],
-
-  //---------------------------------------------------------------------
-  // Command HTML
-  //
-  // This function returns a string containing the HTML used for
-  // editing actions.
-  //
-  // The "isEvent" parameter will be true if this action is being used
-  // for an event. Due to their nature, events lack certain information,
-  // so edit the HTML to reflect this.
-  //---------------------------------------------------------------------
-
-  html(isEvent, data) {
+  html() {
     return `
-<div style="width: 85%;">
-	<span class="dbminputlabel">Command/Event</span><br>
-	<select id="source" class="round">
-		<optgroup id="commands" label="Commands"></optgroup>
-		<optgroup id="events" label="Events"></optgroup>
-	</select><br>
-	<span class="dbminputlabel">Call Type</span><br>
-	<select id="type" class="round">
-		<option value="true" selected>Wait for Completion</option>
-		<option value="false">Run Simultaneously</option>
-	</select>
+<div style="float: left; width: 85%; padding-top: 20px;">
+<span class="dbminputlabel">Tipo de fonte</span><br>
+  <select id="sourcetype" class="round" onchange="glob.onChange1(this)">
+    <option value="0" selected>Escolha da lista</option>
+    <option value="1">Inserir o ID do comando/evento</option>
+    <option value="2">Inserir o nome do comando/evento</option>
+  </select>
+</div>
+<div id="info1"; style="float: left; width: 85%; padding-top: 20px; display: none;">
+<span class="dbminputlabel">Comando/Evento</span><br>
+  <select id="source" class="round">
+    <optgroup id="commands" label="Commands"></optgroup>
+    <optgroup id="events" label="Events"></optgroup>
+  </select>
+</div>
+<div id="info2" style="float: left; width: 85%; padding-top: 20px;">
+<span class="dbminputlabel">ID do comando/evento</span><br>
+  <input id="source2" class="round" type="text" placeholder="">
+</div>
+<div id="info3" style="float: left; width: 85%; padding-top: 20px;">
+<span class="dbminputlabel">Nome do comando/evento</span><br>
+  <input id="source3" class="round" type="text" placeholder="">
+</div>
+<div style="float: left; width: 85%; padding-top: 20px;">
+<span class="dbminputlabel">Tipo de chamada</span><br>
+  <select id="type" class="round">
+  <option value="true" selected>Aguarde a conclusão</option>
+  <option value="false">Executar simultaneamente</option>
+  </select>
 </div>`;
   },
-
-  //---------------------------------------------------------------------
-  // Action Editor Init Code
-  //
-  // When the HTML is first applied to the action editor, this code
-  // is also run. This helps add modifications or setup reactionary
-  // functions for the DOM elements.
-  //---------------------------------------------------------------------
 
   init() {
     const { glob, document } = this;
 
-    const $cmds = glob.$cmds;
-    const coms = document.getElementById("commands");
-    coms.innerHTML = "";
+    const { $cmds } = glob;
+    const coms = document.getElementById('commands');
+    coms.innerHTML = '';
     for (let i = 0; i < $cmds.length; i++) {
       if ($cmds[i]) {
         coms.innerHTML += `<option value="${$cmds[i]._id}">${$cmds[i].name}</option>\n`;
       }
     }
 
-    const $evts = glob.$evts;
-    const evet = document.getElementById("events");
-    evet.innerHTML = "";
+    const { $evts } = glob;
+    const evet = document.getElementById('events');
+    evet.innerHTML = '';
     for (let i = 0; i < $evts.length; i++) {
       if ($evts[i]) {
         evet.innerHTML += `<option value="${$evts[i]._id}">${$evts[i].name}</option>\n`;
       }
     }
-  },
 
-  //---------------------------------------------------------------------
-  // Action Bot Function
-  //
-  // This is the function for the action within the Bot's Action class.
-  // Keep in mind event calls won't have access to the "msg" parameter,
-  // so be sure to provide checks for variable existence.
-  //---------------------------------------------------------------------
+    glob.onChange1 = function onChange1(event) {
+      const sourceType = parseInt(document.getElementById('sourcetype').value, 10);
+      const info1 = document.getElementById('info1');
+      const info2 = document.getElementById('info2');
+      const info3 = document.getElementById('info3');
+
+      switch (sourceType) {
+        case 0:
+          info1.style.display = null;
+          info2.style.display = 'none';
+          info3.style.display = 'none';
+          break;
+        case 1:
+          info1.style.display = 'none';
+          info2.style.display = null;
+          info3.style.display = 'none';
+          break;
+        case 2:
+          info1.style.display = 'none';
+          info2.style.display = 'none';
+          info3.style.display = null;
+          break;
+        default:
+          break;
+      }
+    };
+
+    glob.onChange1(document.getElementById('sourcetype'));
+  },
 
   action(cache) {
     const data = cache.actions[cache.index];
-    const Files = this.getDBM().Files;
+    const { Files } = this.getDBM();
 
-    const id = data.source;
+    let id;
+    if (parseInt(data.sourcetype, 10) === 1) {
+      id = this.evalMessage(data.source2, cache);
+    } else {
+      id = data.source;
+    }
+    if (!id) return console.log('Insira um ID de comando/evento!');
+
+    let name;
+    if (parseInt(data.sourcetype, 10) === 2) {
+      name = this.evalMessage(data.source3, cache);
+    } else {
+      name = data.source;
+    }
+    if (!name) return console.log('Insira um nome de um comando/evento!');
+
+
     let actions;
     const allData = Files.data.commands.concat(Files.data.events);
     for (let i = 0; i < allData.length; i++) {
-      if (allData[i]?._id === id) {
-        actions = allData[i].actions;
-        break;
+      if (parseInt(data.sourcetype, 10) === 0) {
+        if (allData[i] && allData[i]._id === id) {
+          actions = allData[i].actions;
+          break;
+        }
+      }
+      if (parseInt(data.sourcetype, 10) === 1) {
+        if (allData[i] && allData[i]._id === id) {
+          actions = allData[i].actions;
+          break;
+        }
+      }
+      if (parseInt(data.sourcetype, 10) === 2) {
+        if (allData[i] && allData[i].name === name) {
+          actions = allData[i].actions;
+          break;
+        }
       }
     }
-
     if (!actions) {
       this.callNextAction(cache);
       return;
@@ -142,14 +164,6 @@ module.exports = {
     }
   },
 
-  //---------------------------------------------------------------------
-  // Action Bot Mod
-  //
-  // Upon initialization of the bot, this code is run. Using the bot's
-  // DBM namespace, one can add/modify existing functions if necessary.
-  // In order to reduce conflicts between mods, be sure to alias
-  // functions you wish to overwrite.
-  //---------------------------------------------------------------------
 
   mod() {},
 };
